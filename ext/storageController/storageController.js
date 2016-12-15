5b1797d0d0d8e04f33ec5ee6e40c087fda89dd31
jamesgu <jamesgu521@gmail.com>
2016-12-15 11:51:29 -0800

    Fix bug with inserting current url, only insert unique urls

--- ext/storageController/storageController.js
+++ ext/storageController/storageController.js
@@ -89,7 +89,6 @@ var removeUrl = function(urls) {
   getUserlist(function(results) {
     newList = results;
     
-    console.log(newList);
     // Remove any urls given from newList
     urls.forEach(function(url) {
       var pattern = new RegExp(url);
@@ -103,7 +102,7 @@ var removeUrl = function(urls) {
     // Set chrome storage to newList
     chrome.storage.sync.set({ 'userGeneratedBlacklist': newList }, function() {
       console.log('Successfully removed: ', urls);
-      getUserlist((results) => {console.log('new list..', results)});
+      getUserlist((results) => {console.log('new list: ', results)});
     });
   });
 };
